cf429d4031317ac6b60f5f9c8958871e96e332e1
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-03-08 22:51:46 +0100

    test: add unit tests

--- ip.spec.ts
+++ ip.spec.ts
@@ -3,8 +3,12 @@ import { it, describe } from "node:test";
 import assert from "node:assert/strict";
 
 void describe("ip()", async (t) => {
-  void it(`should resolve an IPv4 address`, async () =>
-    assert.deepEqual(await ipv4("localhost"), ["127.0.0.1"]));
-  void it(`should resolve an IPv6 address`, async () =>
-    assert.deepEqual(await ipv6("localhost"), ["::1"]));
+  void it(`should resolve an IPv4 address`, async () => {
+    const ips = await ipv4("one.one.one.one");
+    assert.equal(ips.includes("1.1.1.1"), true);
+  });
+  void it(`should resolve an IPv6 address`, async () => {
+    const ips = await ipv6("one.one.one.one");
+    assert.equal(ips.includes("2606:4700:4700::1111"), true);
+  });
 });
