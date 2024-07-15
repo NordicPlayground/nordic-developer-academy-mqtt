e52baf25a1e7f11e388eb1ad7baa4a5796a893d5
Markus Tacker <m@coderbyheart.com>
2024-07-15 15:52:45 +0200

    feat: implement usage metrics
    This is implemented by parsing the mosquitto debug logs
    for relevant log entries, and writing them to seperate
    log files to be picked up by Log Analytics agent.
    
    This is because there is no plug-in mechanish
    that would allow to hook into the mosquitto process.
    
    Log format is defined in https://learn.microsoft.com/en-us/azure/azure-monitor/agents/data-sources-custom-logs

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,39 @@ FROM ubuntu:22.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 
-RUN \
-    apt-get -q update && \
-    apt-get -yq install software-properties-common && \
-    apt-add-repository ppa:mosquitto-dev/mosquitto-ppa && \
-    apt-get -q update && \
+RUN <<EOT
+    apt-get -q update
+    apt-get -yq install software-properties-common
+    apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+    apt-get -q update
     apt-get -yq install mosquitto
+    mkdir /var/log/academy
+EOT
+
+RUN <<EOT
+    # install Node.js
+    apt-get -yq install curl
+    # Install Node.js
+    curl -O https://nodejs.org/dist/v20.15.1/node-v20.15.1-linux-x64.tar.xz
+    tar -xf node-v20.15.1-linux-x64.tar.xz -C /usr/local --strip-components=1
+    rm node-v20.15.1-linux-x64.tar.xz
+EOT
+
+RUN <<EOT
+    apt-get -yq install supervisor
+EOT
+
 EXPOSE 1883
 EXPOSE 8883
 COPY ./mosquitto.conf /mosquitto.conf
 
+COPY metrics /metrics
+
+RUN chmod 777 /var/log/academy
+RUN chown 105:106 /var/log/mosquitto
+
 VOLUME [ "/etc/cert" ]
 
-CMD [ "mosquitto", "-c", "/mosquitto.conf"]
+COPY supervisord.conf /etc/supervisor/
+
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf"]  
---
+++ metrics/collect-logs.js
@@ -0,0 +1,56 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const logDir = "/var/log/academy";
+const mosquittoLog = "/var/log/mosquitto/mosquitto.log";
+const lastLogFile = path.join(logDir, "lastLog");
+
+const getLogEntries = () => {
+  let lastLog = undefined;
+  try {
+    lastLog = fs.readFileSync(lastLogFile, "utf8").trim();
+  } catch {
+    // No lastLog file
+  }
+
+  try {
+    return fs
+      .readFileSync(mosquittoLog, "utf8")
+      .trim()
+      .split("\n")
+      .map((s) => [s.slice(0, 19), s.slice(21)])
+      .filter((s) => lastLog === undefined || s[0].localeCompare(lastLog) > 0);
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
+const collectMetrics = () => {
+  const nowString = new Date().toISOString().slice(0, 19).replace(/:/g, "");
+  const metricsLogFile = path.join(logDir, `mqtt-${nowString}.log`);
+
+  const logEntries = getLogEntries();
+
+  fs.writeFileSync(
+    lastLogFile,
+    logEntries[logEntries.length - 1]?.[0] ?? nowString,
+    "utf-8"
+  );
+
+  const metrics = [];
+  for (const [timestamp, message] of logEntries) {
+    if (message.includes("CONNACK")) {
+      metrics.push(`${timestamp},mqtt,connect`);
+    }
+    if (message.includes("Received PUBLISH")) {
+      metrics.push(`${timestamp},mqtt,publish`);
+    }
+  }
+  if (metrics.length > 0)
+    fs.writeFileSync(metricsLogFile, metrics.join("\n") + "\n", "utf-8");
+};
+
+setInterval(collectMetrics, 60 * 1000);
+// Wait 10 seconds before starting the first collection
+setTimeout(collectMetrics, 10 * 1000);
